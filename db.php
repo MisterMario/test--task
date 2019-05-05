0814162dc429b7cc0ad09ba8b185e112000e88d1
Mr_Mario <aim.power2@gmail.com>
2019-05-05 18:20:50 +0300

    В класс XMLDB добавлены методы выборки и вставки
    Изменения:
    В index.php содержится отладочный код.
    В таблицу "users" БД добавлены строки с исходными данными.

--- db.php
+++ db.php
@@ -12,6 +12,50 @@ class XMLDB {
     - update
     - delete
   */
+
+  public function __construct() {}
+
+  public function select($table_name, $fields, $rows_num = -1) {
+    $simple_xml_el = simplexml_load_file("database/".$table_name.".xml");
+
+    if (isset($simple_xml_el->row)) {
+      $rows = array();
+      $count = count($simple_xml_el);
+      if ($rows_num != -1 && $rows_num <= $count) $count = $rows_num;
+
+      // Извлечение нужных полей
+      for ($i=0; $i < $count; $i++) {
+        $rows[$i] = array();
+        foreach($fields as $field) {
+          if (isset($simple_xml_el->row[$i]->$field)) {
+            $rows[$i][$field] = (string)$simple_xml_el->row[$i]->$field;
+          }
+        }
+      }
+
+      return $rows;
+    }
+    return null;
+  }
+
+  /*
+    Тут есть небольшой ньюанс:
+    В БД добавятся только те поля, которые укажет разработчик.
+    Есть вариант создать одну строку, которая будет шаблоном для остальных и будет определять структуру строк БД.
+    Чтобы поля которые нужно оставить пустыми - остались пустыми, а не вовсе отсутствовали
+  */
+  public function insert($table_name, $fields_and_values) {
+    $simple_xml_el = simplexml_load_file("database/".$table_name.".xml");
+
+    if (isset($simple_xml_el->row)) {
+      $new_row = $simple_xml_el->addChild("row");
+      foreach($fields_and_values as $field => $value) {
+        $new_row->addChild($field, $value);
+      }
+      file_put_contents("database/".$table_name.".xml", $simple_xml_el->asXML());
+    }
+    return false;
+  }
 }
 
- ?>
+?>
--- index.php
+++ index.php
@@ -2,4 +2,9 @@
 
 // Этот скрипт будет отвечать за показ страницы при обращении к ней любым путем, кроме AJAX
 
+require_once("db.php");
+
+$xmlDB = new XMLDB();
+var_dump($xml_file_content = $xmlDB->select("users", array("login", "password", "email", "name")));
+;
  ?>
